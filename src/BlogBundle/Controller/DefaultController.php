b42885fc5fa9dda478675517ca1878c1760d77e7
bumaha <max@Maxs-MacBook-Pro.local>
2016-03-07 12:08:08 +0100

    article list and crud

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,37 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BlogBundle\Entity\Article;
 
 class DefaultController extends Controller
 {
+    
+    /**
+     * Lists all Article entities.
+     *
+     */
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+    
+        $articles = $em->getRepository('BlogBundle:Article')->findAll();
+    
+        return $this->render('BlogBundle:Default:index.html.twig', array(
+            'articles' => $articles,
+        ));
     }
+    
+
+    /**
+     * Finds and displays a Article entity.
+     *
+     */
+    public function detailAction(Article $article)
+    {
+        return $this->render('BlogBundle:article/detail.html.twig', array(
+            'article' => $article,
+        ));
+    }
+    
+
 }
